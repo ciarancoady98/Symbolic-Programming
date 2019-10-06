d389759141a869f7fbb94d56691a8bb939b5bd08
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-10-06 14:07:56 +0100

    Finished implementation for binary tree swap function

--- Exercises/ExerciseSet3/kb4.pl
+++ Exercises/ExerciseSet3/kb4.pl
@@ -1,3 +1,2 @@
-numeral(0),
-numeral(succ(X)):-numeral(X).
-greater_than(X,Y):-numeral(X)
+greater_than(succ(_),0).
+greater_than(succ(X),succ(Y)):-greater_than(X,Y).
--- Exercises/ExerciseSet3/kb5.pl
+++ Exercises/ExerciseSet3/kb5.pl
@@ -1,1 +1,4 @@
-greater_than(X,Y):-
+swap(leaf(X), leaf(X)).
+swap(tree(X, Y), tree(SwappedY, SwappedX)) :-
+    swap(X, SwappedX),
+    swap(Y, SwappedY).
