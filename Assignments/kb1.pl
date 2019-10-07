9de951f25752113e7a906b4d1d68cd75445ccde4
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-10-07 15:16:45 +0100

    Extended add2 and subtract to support x-y

--- Assignments/kb1.pl
+++ Assignments/kb1.pl
@@ -1,8 +1,10 @@
 numeral(0).
-numeral(s(X)) :- numeral(X).
-numeral(X+Y):-numeral(X),numeral(Y).
-numeral(p(X)) :- numeral(X).
 numeral(-X) :- numeral(X).
+numeral(s(X)) :- numeral(X).
+numeral(p(X)) :- numeral(X).
+numeral(X+Y):-numeral(X),numeral(Y).
+numeral(X-Y) :- numeral(X), numeral(Y).
+
 
 add2(0,0,0).
 add2(0,A,B):-add2(A,0,B).
@@ -16,12 +18,15 @@ add2(s(X),Y,s(Z)) :- add2(X,Y,Z).
 add2(p(X),Y,p(Z)) :- add2(X,Y,Z).
 add2(s(A+B),C,s(D)) :- add2(A+B,C,D).
 add2(A+B,C,D) :- add2(A,B,E),add2(E,C,D).
+add2(A-B,C,D) :- add2(A,-B,E),add2(E,C,D).
 
 minus(0,Z):-Z=0.
 minus(s(p(X)),Z):-minus(X,Z).
 minus(p(s(X)),Z):-minus(X,Z).
 minus(s(X),p(Z)):-minus(X,Z).
 minus(p(X),s(Z)):-minus(X,Z).
+minus(X+Y,Z):-add2(X,Y,P),minus(P,Z).
+minus(X-Y,Z):-add2(-X,Y,Z).
 
 subtract(X,-Y,Z):-add2(X,Y,Z).
 subtract(X,Y,Z):-add2(X,-Y,Z).
