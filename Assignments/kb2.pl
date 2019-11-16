e45215be45b899416ad52758f05dee33006b17e7
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-11-16 18:36:02 +0000

    added question as comment

---
+++ Assignments/kb2.pl
@@ -0,0 +1,12 @@
+% Question 1
+% Write a DCG that accepts strings of the form u2v where u and v are strings
+% over the alphabet {0, 1} such that the number of 0’s in u is twice the number
+% of 1’s in v. For example,
+%
+% ?- s([0,1,0,1,2,0,0,1,0],L).
+%
+%
+
+s(A,B):=
+
+
