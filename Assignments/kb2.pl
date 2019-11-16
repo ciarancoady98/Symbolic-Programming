21d5831e56cf3f502acc0eb8e25531a639a633a6
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-11-16 18:49:35 +0000

    finished question 2

--- Assignments/kb2.pl
+++ Assignments/kb2.pl
@@ -7,6 +7,31 @@
 %
 %
 
-s(A,B):=
+
+
+
+% Question 2
+% (∗) three neighbouring houses that all have a different colour, namely red,
+% blue, and green. People of different nationalities live in the different
+% houses and they all have a different pet.
+%
+% write a DCG that outputs strings [Col1,Nat1,Pet1, Col2,Nat2,Pet2, Col3,Nat3,Pet3]
+% satisyfing (∗), where the nationalities are english, spanish, japanese and
+% the pets are jaguar, snail, zebra. 
+
+s2 --> colour(C1), nationality(N1), pet(P1), colour(C2), nationality(N2), pet(P2), colour(C3), nationality(N3), pet(P3), {C1\=C2, C2\=C3, C1\=C3, N1\=N2, N2\=N3, N1\= N3, P1\=P2, P2\=P3, P1\=P3}.
+
+colour([red]) --> [red].
+colour([blue]) --> [blue].
+colour([green]) --> [green].
+
+nationality([english]) --> [english].
+nationality([spanish]) --> [spanish].
+nationality([japanese]) --> [japanese].
+
+pet([jaguar]) --> [jaguar].
+pet([snail]) --> [snail]. 
+pet([zebra]) --> [zebra]. 
+
 
 
