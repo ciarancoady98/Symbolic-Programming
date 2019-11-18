ebb144b2b6b0bbcadd589d8fd8e3ee86c5371d74
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-11-18 16:53:00 +0000

    added comments to explain renaming of functions

--- Assignments/kb2.pl
+++ Assignments/kb2.pl
@@ -1,3 +1,5 @@
+% Author: Ciarán Coady
+
 % Question 1
 % Write a DCG that accepts strings of the form u2v where u and v are strings
 % over the alphabet {0, 1} such that the number of 0’s in u is twice the number
@@ -23,6 +25,8 @@ ones(Count) --> [0], ones(Count).
 % write a DCG that outputs strings [Col1,Nat1,Pet1, Col2,Nat2,Pet2, Col3,Nat3,Pet3]
 % satisyfing (∗), where the nationalities are english, spanish, japanese and
 % the pets are jaguar, snail, zebra. 
+%
+% renamed to s2 to avoid conflict with question 1
 
 s2 --> colour(C1), nationality(N1), pet(P1), colour(C2), nationality(N2), pet(P2), colour(C3), nationality(N3), pet(P3), {C1\=C2, C2\=C3, C1\=C3, N1\=N2, N2\=N3, N1\= N3, P1\=P2, P2\=P3, P1\=P3}.
 
@@ -47,6 +51,7 @@ pet([zebra]) --> [zebra].
 % keep track of the current sum recursively add elements to our resulting list until
 % the accumulator value hits 0. If we get to 0 then return list.
 %
+% renamed to sum to avoid conflict with other questions
 
 sum(0) --> [].
 sum(Number)--> [X], {mkList(Number,Alphabet), member(X,Alphabet), Accumulator is Number-X, Accumulator>=0}, sum(Accumulator).
